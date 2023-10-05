12401f76bd8d74af79e4acfb69c35abd3506eafa
botcarola <celerner4@gmail.com>
2023-10-05 14:16:12 -0300

    preentrega-1

---
+++ preentregas/primer-preentrega.js
@@ -0,0 +1,30 @@
+// ESTOS PUNTOS DE TAREA COMPRENDEN TODO LO VISTO DURANTE LAS CLASE 1, 2, 3, 4
+// EL PRIMER DESAFÍO COMPRENDE: VARIABLES, CONDICIONALES, BUCLES, FUNCIONES
+// ESO SIGNIFICA QUE TODO EJERCICIO QUE CONTENGA MÉTODOS O DATOS QUE NO FUERON EN CLASES, NO SERÁN EVALUADOS
+// RECUERDEN QUE EL PRIMER DESAFÍO INVOLUCRA QUE INTEGREN TODO LO VISTO
+// ACLARACIÓN: ESTE DESAFÍO NO NECESARIAMENTE PODRÁ ESTAR RELACIONADO CON EL DESAFÍO FINAL
+
+// CADA PUNTO EN REALIDAD ES UN EJERCICO ÚNICO QUE CONTIENE TODO LO NECESARIO PARA APROBAR
+// LOS TEXTOS DE LOS MENSAJES SON A CRITERIO PERSONAL, PERO DEBEN SEGUIR LA LÓGICA PLANTEADA
+
+// 1) PRIMER CASO:
+// - Pregunte al usuario su nombre a través de un prompt. 
+// - Con el valor de su nombre, envíe un mensaje a través de un alert que le de la bienvenida. Ej: "Bienvenido a nuestro ecommerce Javier."
+// - Pregúntele al usuario, con un nuevo prompt, qué categoría de producto le interesaría comprar, ej: "¿Qué desea comprar? Elija la opción 1 si son remeras, opción 2 si son pantalones". La frase y la forma de introducir los datos queda a criterio de ustedes. Lo necesario es que a partir del valor ingresado, se valide qué mostrar luego.
+// - Valide el valor ingresado. En el caso de que sea, por ejemplo, 1, muestre por confirm el artículo remera, con un mensaje de este estilo: "Usted eligió remera. Su precio es de $x. Desea comprar este artículo?"
+// - En el caso de cancelar, terminar el algoritmo con un nuevo alert: "Muchas gracias por su visita"
+// - Si el usuario decide aceptar la compra, se deberá mediante un bucle, reproducir tres mensajes distintos: El primero será: "Ingrese su dirección para poder realizar el envío del pedido", el segundo: "Su artículo será enviado a: - dirección-" y el tercero: "Muchas gracias por su compra".
+// FIN DEL EJERCICIO
+// RECUERDEN ENCAPSULAR LA LÓGICA DENTRO DE FUNCIONES
+// PUEDEN HACER UNA FUNCIÓN QUE CONTENGA UN ALERT Y QUE EL TEXTO DEL ALERT SEA PASADO COMO PARÁMETRO, COSA DE NO REPETIR LOS ALERTS Y APRENDER A HACER LÓGICA REUTILIZABLE.
+
+// 2) SEGUNDO CASO:
+// - Pregunte al usuario su nombre a través de un prompt. 
+// - Con el valor de su nombre, envíe un mensaje a través de un alert que le de la bienvenida. Ej: "Bienvenido a la red Javier"
+// - A través de un bucle, enviar 5 prompts con mensajes diferentes. Imaginen que cada prompt envía un "tweet" y que, el usuario a través del input, tiene que calificarlo. Puede calificarlo con: "me gusta" y "no me gusta". 
+// Si el valor ingresado es "me gusta", mediante una variable acumuladora, se validará la cantidad. Si el valor es "no me gusta", también se precisará de otra variable que acumule ese resultado.
+// - Luego del bucle, se debe enviar un alert que visualice la cantidad de "me gusta" y "no me gusta". Ej: "Del siguiente contenido, te gustaron 3 y no te gustaron 2". Los números deben representar a los valores de las variables acumuladas.
+// FIN DEL EJERCICIO
+// RECUERDEN ENCAPSULAR LA LÓGICA DENTRO DE FUNCIONES
+// PUEDEN HACER UNA FUNCIÓN QUE CONTENGA UN PROMPT Y QUE EL TEXTO DEL ALERT SEA PASADO COMO PARÁMETRO, COSA DE NO REPETIR LOS ALERTS Y APRENDER A HACER LÓGICA REUTILIZABLE.
+
