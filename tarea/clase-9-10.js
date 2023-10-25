0fcf5c71983662a3ff3b1dd3962fca642687b0d7
botcarola <celerner4@gmail.com>
2023-10-25 12:19:45 -0300

    tarea clase dom y eventos

---
+++ tarea/clase-9-10.js
@@ -0,0 +1,10 @@
+
+// 1. **MODO OSCURO (prototipo):** Crea un botón que, al hacer click, cambie el color de fondo del body.
+
+// 2. **Contador de clicks:** Implementa un contador que se incremente cada vez que se haga click en un botón y muestre el resultado en el DOM de el valor incrementado en cada click. Para poder llevarlo a cabo, precisarán de una variable acumuladora por fuera del evento.
+
+// 3. **Ocultar y mostrar elementos:** Crea un botón que oculte y muestre un parrafo cuando se hace click. Podrán hacerlo con toggle o un if que verifique el estado del display del elemento.
+
+// 4. **Validación de formulario:** Diseña un formulario con el campo nombre y apellido, valide si estos campos de entrada están vacíos cuando el usuario intenta enviar el formulario. Muestra un mensaje de error en el caso de que sea así. Si los campos están completos, envíe un alert con el mensaje: "Su formulario ha sido enviado".
+
+// 5. **Lista de clientes:** A través de un formulario, el cual deberá tener los siguientes campos: nombre, apellido, telefono y provincida, se deberá poder dar de alta nuevos usuarios. Al enviar el formulario con evento submit, se deberá pushear estos clientes a un array y se deberá generar nodos con sus datos e inyectar estos componentes en el DOM. PISTA: todo debe realizarse dentro del mismo evento.
