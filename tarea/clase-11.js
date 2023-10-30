8d25c0b0d1b7a5e285eabe88259fc888e48ce4bb
botcarola <celerner4@gmail.com>
2023-10-30 10:30:20 -0300

    clase 11 storage: tarea

---
+++ tarea/clase-11.js
@@ -0,0 +1,24 @@
+// **Nivel Básico - Datos Clave/Valor:**
+
+// 1. **LocalStorage - Modo Oscuro:**
+//    - Crea una función que permita cambiar entre el modo oscuro y el modo claro en una página web.
+//    - Utiliza `localStorage` para almacenar el estado actual del modo oscuro (por ejemplo, "dark" o "light").
+//    - Al cargar la página, verifica el valor almacenado en `localStorage` y aplica el modo correspondiente.
+
+// 2. **SessionStorage - Usuario Loggeado:**
+//    - Implementa un formulario de inicio de sesión que solicite el nombre de usuario y contraseña.
+//    - Utiliza `sessionStorage` para almacenar si el usuario ha iniciado sesión (por ejemplo, "true" o "false").
+//    - Al cargar la página, verifica el valor almacenado en `sessionStorage` y muestra un mensaje de bienvenida si el usuario está loggeado.
+
+// **Nivel Intermedio - Datos Objeto/Array:**
+
+// 3. **LocalStorage - Lista de Tareas:**
+//    - Crea una aplicación de lista de tareas donde los usuarios puedan agregar y eliminar tareas.
+//    - Utiliza `localStorage` para almacenar una lista de tareas en formato JSON (un array de objetos).
+//    - Permite que los usuarios agreguen nuevas tareas y actualiza la lista en `localStorage`.
+
+// 4. **SessionStorage - Carrito de Compras:**
+//    - Diseña un carrito de compras en el que los usuarios puedan agregar productos.
+//    - Utiliza `sessionStorage` para almacenar el contenido del carrito en formato JSON (un array de objetos).
+//    - Permite a los usuarios agregar y eliminar productos del carrito y actualiza la información en `sessionStorage`.
+
